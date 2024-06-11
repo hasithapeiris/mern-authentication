9323d68fbecdd110433cf269e7d94462809bc33b
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-06-11 13:54:41 +0530

    create custom error handler middleware

--- backend/server/controllers/userController.js
+++ backend/server/controllers/userController.js
@@ -2,6 +2,8 @@
 // route POST /api/users/auth
 // @access Public
 const authUser = (req, res) => {
+  res.status(401);
+  throw new Error("Something went wrong");
   res.status(200).json({ message: "Auth user" });
 };
 
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 5000;
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
