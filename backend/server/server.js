ab2971e2acdd7bd6b58457b327e1428b8458f3f0
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-06-11 13:13:06 +0530

    create a basic testing route

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Server is ready!"));
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
