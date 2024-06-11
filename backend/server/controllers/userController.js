22c96864cee113cb7934d71998c5b08adcf5a1bc
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-06-11 16:07:32 +0530

    create mongoDB connection

--- backend/server/controllers/userController.js
+++ backend/server/controllers/userController.js
@@ -2,9 +2,41 @@
 // route POST /api/users/auth
 // @access Public
 const authUser = (req, res) => {
-  res.status(401);
-  throw new Error("Something went wrong");
   res.status(200).json({ message: "Auth user" });
 };
 
-export { authUser };
+// @desc Register user
+// route POST /api/users
+// @access Public
+const registerUser = (req, res) => {
+  res.status(200).json({ message: "Register user" });
+};
+
+// @desc Logout user
+// route POST /api/users
+// @access Private
+const logoutUser = (req, res) => {
+  res.status(200).json({ message: "Logout user" });
+};
+
+// @desc Get user profile
+// route GET /api/users/id
+// @access Private
+const getUserProfile = (req, res) => {
+  res.status(200).json({ message: "Get user profile" });
+};
+
+// @desc Update user profile
+// route PUT /api/users/id
+// @access Private
+const updateUserProfile = (req, res) => {
+  res.status(200).json({ message: "Update user profile" });
+};
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/server/server.js
+++ backend/server/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
